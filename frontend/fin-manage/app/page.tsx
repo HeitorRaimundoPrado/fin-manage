41cecae3747b1d037cf69d6d0c293002a4bfaf4c
HeitorRaimundoPrado <heitorrdpp@gmail.com>
2023-12-21 17:30:57 -0300

    feat: add landing page

--- frontend/fin-manage/app/page.tsx
+++ frontend/fin-manage/app/page.tsx
@@ -1,113 +1,51 @@
 import Image from 'next/image'
-
+import Link from 'next/link'
+ 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+    <div className="bg-neutral-950 flex flex-col text-white min-h-screen items-center">
+      <h1 className="font-bold text-xl  mt-16">Fin-Manage</h1>
+      <h2 className="text-lg mx-12 text-center mt-4">Your personal finance manager</h2>
+      <div className="w-screen mt-8 flex flex-row">
+        <Image src="/landing-page-1.png" alt="trade market image" width={200} height={200} style={{ borderBottomRightRadius: '20px' }} className="object-cover"/>
+        <p className=" text-xs w-[40%] ml-6">
+          Take control of your financial journey with our intuitive and powerful personal finance manager app. With Fin-Manage, achieving your financial goals become effortless!
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+      <h2 className="text-xl mt-12 mb-6">Key Features</h2>
+      <div className="w-screen mt-8 flex flex-row">
+        <div className="w-[50%] mr-6">
+        <h2 className="text-center mb-2">Track Your Expenses</h2>
+        <p className="ml-2 text-xs text-right">
+          Reliably track how much you’re spending monthly to have your expenses under control
+        </p>
         </div>
+        <Image src="/landing-page-2.png" alt="trade market image" width={200} height={200} style={{ borderBottomLeftRadius: '20px' }} className="object-cover"/>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-screen mt-8 flex flex-row mb-5">
+        <Image src="/landing-page-3.png" alt="saving goals image" width={200} height={200} style={{ borderBottomRightRadius: '20px' }} className="object-cover"/>
+        <div className=" w-[50%] ml-6">
+          <h2 className="text-center mb-2">Smart Saving Goals</h2>
+          <p className=" text-xs mr-2">
+            Define your dreams and let Fin-Manage help you achieve them. Set up savings goals, track your progress, and celebrate milestones along the way.
+          </p>
+        </div>
+        
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-screen mt-8 flex flex-row">
+        <div className="w-45 mr-6">
+        <h2 className="text-center mb-2">Secutity and Privacy</h2>
+        <p className="ml-2 text-xs text-right">
+            Your financial data is sensitive, and we take its security seriously. Fin-Manage employs state-of-the-art encryption and security measures to ensure that your information remains confidential and protected.
+        </p>
+        </div>
+        <Image src="/landing-page-4.png" alt="security and privacy image" width={210} height={210} style={{ borderBottomLeftRadius: '20px', }} className="object-cover"/>
       </div>
-    </main>
+      <h2 className="text-2xl mt-20">Start Now</h2>
+      <Link href="/signup" className="mt-8 bg-cyan-400 p-2 rounded-md w-40 text-center mb-12">Sign Up</Link>
+      
+
+    </div>
+
   )
 }
